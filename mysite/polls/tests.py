fe905e1797f1c4904558fc8a6ec402946defd1d5
ALessandro <alessandrodea22@gmail.com>
2014-07-25 16:09:39 +0200

    tutorial

--- mysite/polls/tests.py
+++ mysite/polls/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
 
 # Create your tests here.
+import datetime
+from django.utils import timezone
+from django.test import TestCase
+from polls.models import Poll
+
+
+class PollMethodTests(TestCase):
+
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
